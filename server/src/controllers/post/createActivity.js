acd737bd6fda71596af36adf33c2796fa4af28fb
JONAAVILA <jona_1969@hotmail.com>
2023-10-27 18:44:09 -0300

    'post activity create'

--- server/src/controllers/post/createActivity.js
+++ server/src/controllers/post/createActivity.js
@@ -1,12 +1,20 @@
 const createActivity = require('../../handlers/postHandlers/activityPost.js');
 
 const postActivity = async (req,res)=>{
-    const { id,
+    try {
+       const { idPais,
             name,
             difficulty,
             duration,
             season 
-        } = req.body;
+        } = req.body; 
+        
+        const resPostActvity = await createActivity(idPais,name,difficulty,duration,season);
+        return res.status(200).json(resPostActvity);
+    } catch (error) {
+        return res.status(400).json({error:error.messsage});
+    }
+    
 
 }
 
--- server/src/handlers/postHandlers/activityPost.js
+++ server/src/handlers/postHandlers/activityPost.js
@@ -1,7 +1,18 @@
 const { Country, Activity } = require('../../db.js');
 
-const createActivity = async ()=>{
-    
+const createActivity = async (idPais,name,difficulty,duration,season)=>{
+        
+        if(!idPais || !name || !difficulty || !duration || !season){
+            throw new Error("No found parameters");
+        }
+        const newActivity = await Activity.create({
+            idPais: idPais,
+            name: name,
+            difficulty: difficulty,
+            duration: duration,
+            season: season
+        })
+        return newActivity;
 }
 
 module.exports = createActivity;

e6c0342f312c1779a5e87c1ec20c011c70aba1b3
JONAAVILA <jona_1969@hotmail.com>
2023-10-29 20:00:23 -0300

    'create landing'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
+import Landing from './components/landing/Landing';
 
 
 function App() {
 
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/components/landing/Landing.jsx
@@ -0,0 +1,11 @@
+
+const Landing = ()=>{
+    return(
+        <div>
+            <h1>Countries in the word</h1>
+            <button>GO</button>
+        </div>
+    )
+}
+
+export default Landing;

e544f759b3382b1c9d117e0849fb8736f53e6aa3
JONAAVILA <jona_1969@hotmail.com>
2023-12-27 00:13:08 -0300

    'validate inputs'

--- client/src/view/form/validate.js
+++ client/src/view/form/validate.js
@@ -1,8 +1,17 @@
 export default function validate (inputs){
-    const erro = {}
+    const errors = {}
     const regexNameCountry = /^[a-zA-Z]+$/;
 
-    const validateNameCountry = inputs.name.length && inputs.name.length > 1 && regexNameCountry
-    
+    const validateNameCountry = inputs.name.length && inputs.name.length > 1 && regexNameCountry.test(inputs.name)
+    if(inputs.name && !validateNameCountry){
+        errors['name'] = "Enter a valid name"
+    }else{
+        errors['name'] = ""
+    }
 
+    const regexDifficulty = /^(0?|[1-9]|[1][0-9]|24)$/
+    const validateDifficulty = regexDifficulty.test(inputs.difficulty)
+    if(!validateDifficulty && inputs.difficulty){
+        errors['difficulty'] = ""
+    }
 }

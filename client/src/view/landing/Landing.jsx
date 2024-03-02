cd745c2d36215facaf63f5766ae9922188050f81
JONAAVILA <jona_1969@hotmail.com>
2024-03-01 23:50:57 -0300

    style with css landing page

--- client/src/view/landing/Landing.jsx
+++ client/src/view/landing/Landing.jsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom'
+import './Landing.css';
 
 
 const Landing = ()=>{
     return(
-        <div>
-            <h1>Countries in the word</h1>
-            <Link to="/home" >
-               <button>GO</button> 
-            </Link>
-            
+        <div className='box_landing'>
+           <div className='landing_tittle' >
+                <h1>Countries in the word</h1>
+           </div>
+           <div className='landing_button' >
+                <Link to="/home" >
+                    <button>GO</button> 
+                </Link>  
+           </div> 
         </div>
+        
+         
     )
 }
 
